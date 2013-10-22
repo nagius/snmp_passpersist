20ab8622364548878b6f0d52283979cd77526ae4
Robert Oliveira <robert_oliveira@atlantico.com.br>
2013-10-22 15:15:51 -0300

    adds GPL license reference

--- example/settings.py
+++ example/settings.py
@@ -1,7 +1,27 @@
+# -*- coding:utf-8 -*-
+
+'''@package settings.py
+
+Recover settings from a standard ini/conf file
+Author: Robert Oliveira <olivecoder@gmail.com>
 '''
-settings.py
-Recover settings from a standard ini file
-'''
+
+###########################################################################
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###########################################################################
 
 import ConfigParser
 import tempfile
@@ -10,6 +30,11 @@ import shutil
 
 
 class Settings():
+    '''Base class to read and write ini files
+
+    define FNAME in child class
+    '''
+
     FNAME = None
 
     def __init__(self):
